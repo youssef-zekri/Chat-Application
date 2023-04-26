692048418df7f519c4361a31f83852c7d341021d
zekri youssef <youssef.essedik.zekri@gmail.com>
2023-04-26 18:05:30 +0100

    edit side bar

--- app/src/components/SideBar/SideBar.tsx
+++ app/src/components/SideBar/SideBar.tsx
@@ -1,16 +1,41 @@
-import React from 'react'
-import  User  from '../../assets/images/user.png'
-import NavLinks from '../NavLinks/NavLinks'
+import React, { useState } from "react";
+import User from "../../assets/images/user.png";
+import NavLinks from "../NavLinks/NavLinks";
 export default function SideBar() {
+  const Links: { icon: string; path: string; message: string , name?:any }[] = [
+    { icon: "fa-solid fa-house", path: "/", message: "Home" },
+    { icon: "fa-regular fa-user", path: "/profile", message: "Profile" },
+    { icon: "fa-regular fa-message", path: "/chat", message: "Chat" },
+    { icon: "fa-solid fa-gears", path: "/settings", message: "Settings" },
+    { icon: "fa-solid fa-arrow-right-from-bracket", path: "/Auth", message: "Log out" , name : 'logOut' },
+  ];
+  const [sideBar, setSideBar] = useState(false);
+  const [darkMode, setDarkMode] = React.useState(true);
   return (
-    <div className='SideBar'>
-        <img src={User}  className='SideBar__UserPhoto'/>
-        <p className='SideBar__UserName'>Youssef Zekri</p>
-        <NavLinks icon={<i className="fa-solid fa-house"></i>} path={"/"} message={'Home'} />
-        <NavLinks icon={<i className="fa-regular fa-user"></i>} path={"/profile"} message={'Profile'} />
-        <NavLinks icon={<i className="fa-regular fa-message"></i>} path={"/chat"} message={'Chat'} />
-        <NavLinks icon={<i className="fa-solid fa-gears"></i>} path={"/settings"} message={'Settings'} />
-        <NavLinks icon={<i className="fa-solid fa-arrow-right-from-bracket"></i>} path={"/Auth"} message={'Log out'} />
+    <div className={sideBar? "SideBar SideBar--active" : "SideBar"}>
+
+      {!sideBar ? (
+        <>
+          <img src={User} className="SideBar__UserPhoto" />
+          <p className="SideBar__UserName">Youssef Zekri</p>
+        </>
+      ) : <i className="fa-brands fa-slack SideBar__logo"></i>}
+
+      <i
+        className={"fa-solid SideBar__Toggler fa-angle-left"}
+        id={sideBar ? "SideBar__Toggler--active" : "SideBar__Toggler"}
+        onClick={() => setSideBar(!sideBar)}
+      ></i>
+      {Links.map((link: { icon: string; path: string; message: string , name?:any }, i: Number) => (
+        <NavLinks
+          key={`link ${i}`}
+          icon={<i className={link.icon}></i>}
+          path={link.path}
+          message={sideBar ? '':link.message}
+          name = {link.name}
+        />
+      ))}
+
     </div>
-  )
+  );
 }
